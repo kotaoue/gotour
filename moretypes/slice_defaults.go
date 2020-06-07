97599a8269295f4ce8ed255ae91e590040a076f5
kotaoue <kotaoue@gmail.com>
2020-06-07 16:41:08 +0900

    Add [:].

--- moretypes/slice_defaults.go
+++ moretypes/slice_defaults.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	s := []int{2, 3, 5, 7, 11, 13}
 
+	s = s[:]
+	fmt.Println(s)
+
 	s = s[1:4]
 	fmt.Println(s)
 
@@ -12,5 +15,8 @@ func main() {
 	fmt.Println(s)
 
 	s = s[1:]
+	fmt.Println(s)
+
+	s = s[:]
 	fmt.Println(s)
 }
